561e2d91e00a53625fdf9f64044a175cdfeb49d5
Guillem Moreso <guillem.moreso@gmail.com>
2019-09-09 17:34:59 +0200

    Eslint Arrow functions Fixed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  try {
+    res.render("index", { title: "OkeyToPlay" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
