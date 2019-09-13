d864d02d17045bf3b8c79328e98c631e125a2da9
admin <jsbparra@gmail.com>
2019-09-13 18:29:14 +0200

    it.1 vista modificada bands/band.hbs

--- models/Band.js
+++ models/Band.js
@@ -4,16 +4,17 @@ const { Schema } = mongoose;
 
 const bandSchema = new Schema(
   {
-    genre: { type: String, required: true },
     // username:{ type: String, required: true },
     // email: { type: String, required: true, unique: true },
     // hashedPassword: { type: String, required: true },
     // bandName: { type: String },
     // establishmentName: { type: String },
+    image: String,
+    genre: { type: String, required: true },
+    bandMembers: [{ artistName: String, artistInstrument: String }],
     web: { type: String },
     instagramProfile: String,
     facebookProfile: String,
-    bandMembers: [{ artistName: String, artistInstrument: String }],
   },
   {
     timestamps: {
