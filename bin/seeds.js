83ee2d223e97f1ed4cd9a7e6dc883f5b62fa3686
Guillem Moreso <guillem.moreso@gmail.com>
2019-09-13 10:00:52 +0200

    Seed WIP Mongo Atlasdata

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,78 +1,78 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const mongoose = require("mongoose");
-const Event = require("../models/Event");
-const User = require("../models/User");
-const Establishment = require("../models/Establishment");
-const Band = require("../models/Band");
+const mongoose = require('mongoose');
+const Event = require('../models/Event');
+const User = require('../models/User');
+const Establishment = require('../models/Establishment');
+const Band = require('../models/Band');
 
 (async () => {
   try {
     const connection = await mongoose.connect(`${process.env.MONGODB_URI}`, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
     });
     console.log(
-      `Connected to Mongo! Database name: "${connection.connections[0].db.s.databaseName}"`
+      `Connected to Mongo! Database name: "${connection.connections[0].db.s.databaseName}"`,
     );
     connection.connections[0].db.dropDatabase();
   } catch (err) {
-    console.log("Error connecting to Mongo database.", err);
+    console.log('Error connecting to Mongo database.', err);
   }
 })();
 
 const events = [
   {
     schedule: 2019 - 12 - 11,
-    description: "El mejor bar en el barrio del Poblenou",
-    bandName: "Metallica",
+    description: 'El mejor bar en el barrio del Poblenou',
+    bandName: 'Metallica',
     price: 10,
-    durationMins: 120
+    durationMins: 120,
   },
   {
     schedule: 2019 - 12 - 10,
-    description: "El mediocre bar en el barrio del Poblenou",
-    bandName: "Ozzy the Busy",
+    description: 'El mediocre bar en el barrio del Poblenou',
+    bandName: 'Ozzy the Busy',
     price: 1,
-    durationMins: 20
+    durationMins: 20,
   },
   {
     schedule: 2019 - 12 - 12,
-    description: "El mejor bar en el barrio de la Mina",
-    bandName: "Yustin Vieva",
+    description: 'El mejor bar en el barrio de la Mina',
+    bandName: 'Yustin Vieva',
     price: 10,
-    durationMins: 20
-  }
+    durationMins: 20,
+  },
 ];
 
 const users = [
   {
-    username: "joeDoe",
-    email: "joeDoe@gmail.com",
-    hashedPassword: "abcd"
-  }
+    username: 'joeDoe',
+    email: 'joeDoe@gmail.com',
+    hashedPassword: 'abcd',
+  },
 ];
 
 const establishments = [
   {
-    description: "Bar de Copas centrico....",
-    website: "machinebar.com",
-    instagramProfile: "machinbarig.com",
-    facebookProfile: "machinebarfb.com",
-    street: "carrer pujades 21",
-    city: "barcelona",
-    zip: "08022",
-    capacity: 100
-  }
+    description: 'Bar de Copas centrico....',
+    website: 'machinebar.com',
+    instagramProfile: 'machinbarig.com',
+    facebookProfile: 'machinebarfb.com',
+    street: 'carrer pujades 21',
+    city: 'barcelona',
+    zip: '08022',
+    capacity: 100,
+  },
 ];
 
 const bands = [
   {
-    description: "Banda de genero punk",
-    website: "punkies.com",
-    instagramProfile: "punkisig.com",
-    facebookProfile: "punkiesfb.com",
-    bandMembers: [{ artistName: "pepe" }, { artistInstrument: "juan" }]
-  }
+    description: 'Banda de genero punk',
+    website: 'punkies.com',
+    instagramProfile: 'punkisig.com',
+    facebookProfile: 'punkiesfb.com',
+    bandMembers: [{ artistName: 'pepe' }, { artistInstrument: 'juan' }],
+  },
 ];
 // db.bands
 //   .drop()
@@ -112,7 +112,7 @@ const bands = [
 
 Event.create(events)
   .then(event => {
-    console.log("inserted event ", event);
+    console.log('inserted event ', event);
     mongoose.connection.close();
   })
   .catch(err => {
@@ -122,7 +122,7 @@ Event.create(events)
 
 User.create(users)
   .then(user => {
-    console.log("inserted user ", user);
+    console.log('inserted user ', user);
     mongoose.connection.close();
   })
   .catch(err => {
@@ -132,7 +132,7 @@ User.create(users)
 
 Establishment.create(establishments)
   .then(establishment => {
-    console.log("inserted establishment ", establishment);
+    console.log('inserted establishment ', establishment);
     mongoose.connection.close();
   })
   .catch(err => {
@@ -142,7 +142,7 @@ Establishment.create(establishments)
 
 Band.create(bands)
   .then(band => {
-    console.log("inserted band ", band);
+    console.log('inserted band ', band);
     mongoose.connection.close();
   })
   .catch(err => {
