95d535c88caea8ae8fadb5f977b8ea3ff3dafe22
Xavi <xavigallardo@gmail.com>
2019-09-14 01:05:20 +0200

    WIP pasar js de profile-create HBS a js

--- routes/events.js
+++ routes/events.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const Event = require('../models/Event');
 const User = require('../models/User');
 
-const mongoose = require('mongoose');
 
 const router = express.Router();
 
 /* GET Renders available events */
 router.get('/', (req, res, next) => {
   Event.find()
-    .then(events => {
+    .then((events) => {
       console.log('events ', events);
       res.render('events', { events });
     })
