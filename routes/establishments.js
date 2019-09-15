f9069516af15803861a11054fc7d5ed42c00c605
Guillem Moreso <guillem.moreso@gmail.com>
2019-09-15 19:52:03 +0200

    Establishment route Update

--- routes/establishments.js
+++ routes/establishments.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const Establishment = require('../models/Establishment');
-
-
 const mongoose = require('mongoose');
+const User = require('../models/User');
+
+const Establishment = require('../models/Establishment');
 
 const router = express.Router();
 
@@ -16,28 +16,84 @@ router.get('/', (req, res, next) => {
     .catch(next);
 });
 
-/* GET Renders event information */
-router.get('/:establishmentId', (req, res, next) => {
-  const { establishmentId } = req.params;
-
-  Establishment.findById(establishmentId)
-    .then(establishments => {
-      res.render('establishments/show', { establishments });
-    })
-    .catch(next);
+/* GET Renders new establishment */
+router.get('/new', (req, res, next) => {
+  res.render('establishments/new');
 });
 
-/* POST Renders event information */
-router.post('/:establishmentId', (req, res, next) => {
-  const { establishmentId } = req.params;
-  Establishment.find(
-    { _id: establishmentId }
-      .then(establishments => {
-        console.log('establishments ', establishments);
-        res.redirect(`/establishments/${establishmentId}`);
-      })
-      .catch(next),
-  );
+/* POST for the new establishment */
+router.post('/new', async (req, res, next) => {
+  const {
+    name,
+    description,
+    website,
+    instagramProfile,
+    facebookProfile,
+    street,
+    city,
+    zip,
+    capacity,
+    avatar,
+  } = req.body;
+  const actualUserEmail = req.session.currentUser.email;
+  console.log('email: ', actualUserEmail);
+  try {
+    let newEstablishment;
+    let updatedUser;
+    newEstablishment = await Establishment.create({
+      name,
+      description,
+      website,
+      instagramProfile,
+      facebookProfile,
+      street,
+      city,
+      zip,
+      capacity,
+      avatar,
+    });
+    const userFound = await User.findOne({ email: actualUserEmail });
+    updatedUser = await User.updateOne(
+      { _id: userFound._id },
+      {
+        $set: {
+          'role.establishment': true,
+          establishment: newEstablishment,
+        },
+      },
+      {
+        new: true,
+      },
+    );
+
+    res.redirect('/user/profile');
+  } catch (error) {
+    next(error);
+  }
 });
+
+// /* GET Renders event information */
+// router.get('/:establishmentId', (req, res, next) => {
+//   const { establishmentId } = req.params;
+
+//   Establishment.findById(establishmentId)
+//     .then(establishments => {
+//       res.render('establishments/show', { establishments });
+//     })
+//     .catch(next);
+// });
+
+// /* POST Renders event information */
+// router.post('/:establishmentId', (req, res, next) => {
+//   const { establishmentId } = req.params;
+//   Establishment.find(
+//     { _id: establishmentId }
+//       .then(establishments => {
+//         console.log('establishments ', establishments);
+//         res.redirect(`/establishments/${establishmentId}`);
+//       })
+//       .catch(next),
+//   );
+// });
 
 module.exports = router;
