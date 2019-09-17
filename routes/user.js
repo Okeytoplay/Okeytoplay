7fea864bb1389990df8a662f749c97af9434aac3
Guillem Moreso <guillem.moreso@gmail.com>
2019-09-17 11:06:15 +0200

    user section update profiles and Crud creation

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt'); // Bcrypt to encrypt passwords
 const User = require('../models/User'); // User Model
 const Band = require('../models/Band'); // Band Model
 const Event = require('../models/Event'); // Event Model
+const Establishment = require('../models/Establishment'); // Band Model
+
 const {
   checkFields,
   checkEmailAndPasswordNotEmpty,
@@ -13,9 +15,129 @@ const { fechaDeHoy } = require('../public/javascripts/fecha');
 
 const router = express.Router();
 
-router.get('/profile-create', checkIfLoggedIn, (req, res, next) => {
-  res.render('user/profile-create');
+router.post('/profile', checkIfLoggedIn, async (req, res, next) => {
+  const actualUserEmail = req.session.currentUser.email;
+  console.log(actualUserEmail);
+  const userFound = await User.findOne({ email: actualUserEmail }).populate(
+    'band establishment',
+  );
+  // const userID = userFound._id;
+  try {
+    // const user = await User.findById(userID);
+    // res.render('user/profile', { userFound, title: 'Profile' });
+    const role = ['Groupie'];
+    if (userFound.role.band) {
+      role.push('Band');
+    }
+    if (userFound.role.establisment) {
+      role.push('Establishment');
+    }
+    // res.render('user/profile', userFound, role);
+    res.render('user/profile', { userFound, role });
+  } catch (error) {
+    next(error);
+  }
+  // res.render('user/profile');
 });
+
+router.get('/profile', checkIfLoggedIn, async (req, res, next) => {
+  const actualUserEmail = req.session.currentUser.email;
+  console.log(actualUserEmail);
+  const userFound = await User.findOne({ email: actualUserEmail }).populate(
+    'band establishment',
+  );
+  // const userID = userFound._id;
+  try {
+    // const user = await User.findById(userID);
+    // res.render('user/profile', { userFound, title: 'Profile' });
+    // res.render('user/profile', userFound, role);
+    res.render('user/profile', { userFound });
+  } catch (error) {
+    next(error);
+  }
+  // res.render('user/profile');
+});
+
+/* GET Renders available events of the user-> Show all the events of the user */
+router.get('/events', checkIfLoggedIn, async (req, res, next) => {
+  const actualUserEmail = req.session.currentUser.email;
+  console.log(actualUserEmail);
+  const userFound = await User.findOne({ email: actualUserEmail }).populate(
+    'establishment',
+  );
+  console.log('UserFound', userFound);
+  if (userFound.role.establishment === false) {
+    req.flash('error', 'No se ha encontrado que tengas ningún Local');
+    res.redirect('/profile');
+  } else {
+    const userEstablishmentID = userFound.establishment._id;
+    const fechaActual = await fechaDeHoy();
+    const events = await Event.find({
+      establishment: userEstablishmentID,
+    }).sort('schedule');
+    console.log('EVENTOS ORDENADOS por fecha del ESTABLISHMENT: ', events);
+    try {
+      console.log('FECHA ', fechaActual);
+      console.log('events ', events);
+      res.render('user/events', { events, fechaActual, userFound });
+    } catch (error) {
+      next(error);
+    }
+  }
+});
+
+/* GET Renders new event -> Show the page to create a new event */
+router.get(
+  '/events/new',
+  checkIfLoggedIn,
+  checkIfEstablishment,
+  async (req, res, next) => {
+    const fechaActual = await fechaDeHoy();
+    try {
+      res.render('user/events/new', { fechaActual });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+/* POST Create NEW EVENT */
+
+router.post(
+  '/events/new',
+  checkIfLoggedIn,
+  checkIfEstablishment,
+  async (req, res, next) => {
+    const { name, description, price, durationMins, schedule } = req.body;
+    const actualUserEmail = req.session.currentUser.email;
+    // const userFound = await User.findOne({ email: actualUserEmail }).populate(
+    //   'establishment',
+    // ); // THIS IS THE CORRECT!!!
+    // ONLY FOR TEST
+    // ONLY FOR TEST Allow to insert Event without ESTABLISHMENT
+    const userFound = await User.findOne({ email: actualUserEmail });
+
+    try {
+      const eventNew = await Event.create({
+        name,
+        description,
+        price,
+        durationMins,
+        schedule,
+        establishmentId: userFound.establishment,
+      });
+      // Poner FLASH notification
+      req.flash('success', ` El evento ${name} ha sido creado con exito`);
+      res.redirect('/user/events'); // A donde vamos?
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+module.exports = router;
+// router.get('/profile-create', checkIfLoggedIn, (req, res, next) => {
+//   res.render('user/profile-create');
+// });
 
 // router.post('/profile', checkIfLoggedIn, async (req, res, next) => {
 //   let role = ['Groupie'];
@@ -82,107 +204,3 @@ router.get('/profile-create', checkIfLoggedIn, (req, res, next) => {
 //     next(error);
 //   }
 // });
-
-router.post('/profile', checkIfLoggedIn, async (req, res, next) => {
-  const actualUserEmail = req.session.currentUser.email;
-  console.log(actualUserEmail);
-  const userFound = await User.findOne({ email: actualUserEmail }).populate(
-    'band',
-  );
-  // const userID = userFound._id;
-  try {
-    // const user = await User.findById(userID);
-    // res.render('user/profile', { userFound, title: 'Profile' });
-    const role = ['Groupie'];
-    if (userFound.role.band) {
-      role.push('Band');
-    }
-    if (userFound.role.establisment) {
-      role.push('Establishment');
-    }
-    // res.render('user/profile', userFound, role);
-    res.render('user/profile', { userFound, role });
-  } catch (error) {
-    next(error);
-  }
-  // res.render('user/profile');
-});
-
-router.get('/profile', checkIfLoggedIn, async (req, res, next) => {
-  const actualUserEmail = req.session.currentUser.email;
-  console.log(actualUserEmail);
-  const userFound = await User.findOne({ email: actualUserEmail }).populate(
-    'band',
-  );
-  // const userID = userFound._id;
-  try {
-    // const user = await User.findById(userID);
-    // res.render('user/profile', { userFound, title: 'Profile' });
-    // res.render('user/profile', userFound, role);
-    res.render('user/profile', { userFound });
-  } catch (error) {
-    next(error);
-  }
-  // res.render('user/profile');
-});
-
-/* GET Renders available events of the user-> Show all the events of the user */
-router.get('/events', checkIfLoggedIn, async (req, res, next) => {
-  const actualUserEmail = req.session.currentUser.email;
-  console.log(actualUserEmail);
-  const userFound = await User.findOne({ email: actualUserEmail }).populate('establishment');
-  console.log('UserFound', userFound);
-  if (userFound.role.establishment === false) {
-    req.flash('error', 'No se ha encontrado que tengas ningún Local');
-    res.redirect('/profile');
-  } else {
-    const userEstablishmentID = userFound.establishment._id;
-    const fechaActual = await fechaDeHoy();
-    const events = await Event.find({ establishment: userEstablishmentID }).sort('schedule');
-    console.log('EVENTOS ORDENADOS por fecha del ESTABLISHMENT: ', events);
-    try {
-      console.log('FECHA ', fechaActual);
-      console.log('events ', events);
-      res.render('user/events', { events, fechaActual, userFound });
-    } catch (error) {
-      next(error);
-    }
-  }
-});
-
-/* GET Renders new event -> Show the page to create a new event */
-router.get('/events/new', checkIfLoggedIn, checkIfEstablishment, async (req, res, next) => {
-  const fechaActual = await fechaDeHoy();
-  try {
-    res.render('user/events/new', { fechaActual });
-  } catch (error) {
-    next(error);
-  }
-});
-
-/* POST Create NEW EVENT */
-
-router.post('/events/new', checkIfLoggedIn, checkIfEstablishment, async (req, res, next) => {
-  const {
-    name, description, price, durationMins, schedule,
-  } = req.body;
-  const actualUserEmail = req.session.currentUser.email;
-  // const userFound = await User.findOne({ email: actualUserEmail }).populate(
-  //   'establishment',
-  // ); // THIS IS THE CORRECT!!!
-  // ONLY FOR TEST
-  // ONLY FOR TEST Allow to insert Event without ESTABLISHMENT
-  const userFound = await User.findOne({ email: actualUserEmail });
-
-  try {
-    const eventNew = await Event.create({
-      name, description, price, durationMins, schedule, establishmentId: userFound.establishment,
-    });
-    // Poner FLASH notification
-    req.flash('success', ` El evento ${name} ha sido creado con exito`);
-    res.redirect('/user/events'); // A donde vamos?
-  } catch (error) {
-    next(error);
-  }
-});
-module.exports = router;
